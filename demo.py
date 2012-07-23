Fixed minor errors with weighting function.

demo.py
@@ -9,18 +9,19 @@ from partition import *
 from plotgraph import *
 from test_graphs import *
 
+from pyamg.util.utils import get_diagonal
+
 method = 2 # partiton method : 1=isopermetric, 2=spectral
 meshnum = 4
 nodes = 20
 
-dir = 'data/'
 if meshnum==1:
     from pyamg.gallery import mesh
     V,E = mesh.regular_triangle_mesh(nodes,nodes)
 if meshnum==2:
     from scipy.io import loadmat
     graph_names = ['crack_mesh','random_disk_graph','random_disk_graph_1000']
-    mesh = loadmat(dir+graph_names[1])
+    mesh = loadmat('data/'+graph_names[2])
     V=mesh['V']
     E=mesh['E']
 if meshnum==3:
@@ -31,7 +32,7 @@ if meshnum==3:
     V=numpy.vstack(map(numpy.ravel,grid)).T
     E=numpy.vstack((mesh.row,mesh.col)).T
 if meshnum==4:
-    mesh = load_graph(dir+'wing') 
+    mesh = load_graph(0) 
     V=mesh['V']
     E=mesh['E']
 
@@ -42,14 +43,13 @@ if method==1 :
 elif method == 2:
   P1,P2,weights = spectral(A,plot=True)
 
-parts = [P1,P2]
 list_sizes = numpy.array([len(P1),len(P2)])
-
 min_size = min(list_sizes)
 max_size = max(list_sizes)
 min_pos = numpy.where(list_sizes==min_size)[0][0]
 max_pos = numpy.where(list_sizes==max_size)[0][0]
 
+parts = [P1,P2]
 P1 = parts[min_pos]
 P2 = parts[max_pos]
 
@@ -57,29 +57,30 @@ part1 = [P1]
 cuts = [edge_cuts(A,P1)]
 imbalance = [(len(P2)-len(P1)+0.0)/len(P2)]
 
+#v_weights = get_diagonal(A)
+v_weights = numpy.ones(A.shape[0])
+
 rel_scores = [A.shape[0]]
-rel_scores.append(quotient_score(A,P1))
+rel_scores.append(quotient_score(A,P1,v_weights))
 print 'quotient_score : %4.5f' % rel_scores[-1]
 
-while (rel_scores[-1] > 0) and (rel_scores[-1] < rel_scores[-2]) :
-   P1_new,P2_new = improve(A, P1, rel_scores[-1])
+while (rel_scores[-1] < rel_scores[-2]) and (imbalance[-1] < 0.5) :
+   P1,P2 = improve(A, part1[0], rel_scores[-1], v_weights)
+   score = rel_quotient_score(A, part1[0], P1, v_weights)
 
-   score = rel_quotient_score(A,P1,P1_new)
-
-   if not numpy.isfinite(score) : break
+   if not numpy.isfinite(score) or score < 0 : break
 
    print 'relative quotient_score : %4.5f' % score
    rel_scores.append(score)
-   cuts.append(edge_cuts(A, P1_new))
+   cuts.append(edge_cuts(A, P1))
 
-   P1_weight = len(P1_new) + 0.0
-   P2_weight = len(P2_new) + 0.0
+   P1_weight = numpy.sum(v_weights[P1]) + 0.0
+   P2_weight = numpy.sum(v_weights[P2]) + 0.0
    min_weight = min(P1_weight, P2_weight)
    max_weight = max(P1_weight, P2_weight)
    imbalance.append((max_weight-min_weight)/max_weight)
 
-   part1.append(P1_new)
-   P1,P2 = P1_new,P2_new
+   part1.append(P1)
 
 # plot the mesh and partition
 pylab.interactive(True)
@@ -111,6 +112,8 @@ pylab.figure()
 plotperms(A, range(A.shape[0]), title='Original', subplot=221)
 plotperms(A, part1[0], title='Spectral', subplot=222)
 plotperms(A, part1[-1], title='Spectral+Improve', subplot=223)
+
+#cut, metispart = metis(A, 2)
 
 #X = scipy.rand(A.shape[0],2)
 #initial_pos_x = 4*(v+1) + X[:,0]