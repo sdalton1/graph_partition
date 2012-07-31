ff47bc2f73304c34fac38b612c59871d1b6ffd74
Luke Olson <luke.olson@gmail.com>
2012-07-31 12:14:06 -0500

    modified for distance

--- usroads.py
+++ usroads.py
@@ -5,18 +5,25 @@ import pylab
 from pyamg import smoothed_aggregation_solver
 from scipy.io import loadmat
 from scipy.sparse import spdiags
+def dist(a,b):
+    return numpy.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
 mesh=loadmat('usroads-48.mat')
 V=mesh['Problem'][0][0][8][0][0][0]
-Emod=mesh['Problem'][0][0][2].tocsr()
-d = numpy.array(Emod.sum(axis=0)).ravel()
-A = -Emod + spdiags(d,[0],Emod.shape[0],Emod.shape[1])
+A=mesh['Problem'][0][0][2].tocoo()
+if 1:
+    for i in range(A.nnz):
+        A.data[i] = dist(V[A.col[i],:],V[A.row[i],:])
+
+d = numpy.array(A.sum(axis=0)).ravel()
+A = -A + spdiags(d,[0],A.shape[0],A.shape[1])
+
 
 ml = smoothed_aggregation_solver(A, coarse_solver='pinv2',max_coarse=100)
 M = ml.aspreconditioner()
 
 X = scipy.rand(A.shape[0], 2) 
-(eval,evec,res) = lobpcg(A, X, M=None, tol=1e-12, largest=False, \
-        verbosityLevel=0, retResidualNormsHistory=True)
+(eval,evec,res) = lobpcg(A, X, M=M, tol=1e-12, largest=False, \
+        verbosityLevel=0, retResidualNormsHistory=True,maxiter=100)
 
 fiedler = evec[:,1]
 vmed = numpy.median(fiedler)
@@ -26,12 +33,6 @@ v[K]=-1
 K = numpy.where(fiedler>vmed)[0]
 v[K]=1
 
-# plot the mesh and partition
-#trimesh(V,E)
-#sub = pylab.gca()
-#sub.hold(True)
-#sub.scatter(V[:,0],V[:,1],marker='o',s=50,c=v)
 from pylab import *
-scatter(V[:,0],V[:,1],marker='o',s=50,c=v)
+scatter(V[:,0],V[:,1],marker='o',s=10,c=v)
 show()
-#pylab.show()
